abf0b00a1312644510439b8d3e3f627a19119cc3
pb10005 <atihsa.mihs.ne.ho.da@gmail.com>
2019-10-12 01:27:48 +0900

    Fix link

--- pages/local/index.vue
+++ pages/local/index.vue
@@ -1,8 +1,8 @@
 <template>
     <section>
-        <nuxt-link to="explorer">エクスプローラ</nuxt-link>
-        <nuxt-link to="explorer">テキストエディタ</nuxt-link>
-        <nuxt-link to="explorer">マークダウンビューワ</nuxt-link>
+        <nuxt-link to="/explorer">エクスプローラ</nuxt-link>
+        <nuxt-link to="/explorer">テキストエディタ</nuxt-link>
+        <nuxt-link to="/explorer">マークダウンビューワ</nuxt-link>
     </section>
 </template>
 <script>
